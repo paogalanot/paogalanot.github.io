4075e99a495699e1680e008a6455c559ed504c7a
paogalanot <paogalano9514@hotmail.com>
2025-03-21 15:29:15 +0000

    Deploying to gh-pages from @ paogalanot/paogalanot.github.io@571833b3171fa15ef327c0d9ed358f083bb0d7cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-curriculum-vitae",
-          title: "Curriculum Vitae",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Curriculum/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
+          },
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Curriculum/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
