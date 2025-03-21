5ad167edb36f93434a396cb6d1def7668d352dce
paogalanot <paogalano9514@hotmail.com>
2025-03-21 09:40:13 +0000

    Deploying to gh-pages from @ paogalanot/paogalanot.github.io@b7264306d7841d44708cf78b89997c7ca0d608c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%67%61%6C%61%6E%6F@%63%6C%69%6F.%75%63%33%6D.%65%73", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -498,6 +498,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/https://orcid.org/0000-0003-4828-7197", "_blank");
         },
       },{
         id: 'social-rss',
